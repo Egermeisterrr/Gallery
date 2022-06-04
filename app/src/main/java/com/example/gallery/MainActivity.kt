70344d4a28ee438579077b24e7f05af01e7dc6d0
Egor <egor.fokin.3005@mail.ru>
2022-06-04 22:12:52 +0300

    visual bug fix

--- app/src/main/java/com/example/gallery/MainActivity.kt
+++ app/src/main/java/com/example/gallery/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.example.gallery
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.Animator
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gallery.adapter.MyAdapter
 import com.example.gallery.data.DataSource
-import com.example.gallery.model.Image
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,8 +22,16 @@ class MainActivity : AppCompatActivity() {
     private fun init() {
         val myDataset = DataSource().loadImages()
         val recyclerView: RecyclerView = findViewById(R.id.rc_view)
-        val numberOfColumns = 3
+        val numberOfColumns = 2
         recyclerView.layoutManager = GridLayoutManager(this, numberOfColumns)
         recyclerView.adapter = MyAdapter(this, myDataset)
+    }
+
+    fun openImage(view: View) {
+        val imageView : ImageView = findViewById(R.id.image)
+        val cl : ConstraintLayout = findViewById(R.id.cl2)
+
+        imageView.layoutParams.height = cl.layoutParams.height
+        imageView.layoutParams.width = cl.layoutParams.width
     }
 }
